14dd8959e799439b29951abc07a449bfb227684b
jon <jonlov4@gmail.com>
2017-01-18 14:05:38 -0500

    urban dictionary command added

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const cleverbot = require('cleverbot.io');
+const http = require('http');
 
 const chatbot = new cleverbot('ATbnnWU2C4okaHVT', 'GyGHH3l6gnL0jwlyiNf8rx1IncVMcdSO');
 const client = new Discord.Client();
@@ -69,7 +70,7 @@ client.on('message', message => {
           message.channel.send('Invite me to your server! https://discordapp.com/oauth2/authorize?client_id=270625647464939521&scope=bot&permissions=8');
           commandIssued = true;
         }else if (msg === 'help') {
-          message.channel.send('__Charlie Help__ \n*All commands must begin with a mention of @Charlie* \n \n **help** - Full list of commands. \n **ping** - If Charlie is working, replies with \"pong!\" \n **marco** - polo! \n **time** - Shows your computer\'s time. \n **avatar** [arg] - Displays the mentioned user\'s avatar, or your own if no user is mentioned. \n **owner** - Relays the owner of the server. \n **invite** - Want to add Charlie to your own server? \n \n *If you start a message by mentioning Charlie but no command is recognized, Charlie will reply as Cleverbot would!*');
+          message.channel.send('__Charlie Help__ \n*All commands must begin with a mention of @Charlie* \n \n **help** - Full list of commands. \n **ping** - If Charlie is working, replies with \"pong!\" \n **marco** - polo! \n **time** - Shows your computer\'s time. \n **avatar** [user] - Displays the mentioned user\'s avatar, or your own if no user is mentioned. \n **owner** - Relays the owner of the server. \n **invite** - Want to add Charlie to your own server? \n **ud** [word/term] - Look up a word or term on Urban Dictionary. \n \n *If you start a message by mentioning Charlie but no command is recognized, Charlie will reply as Cleverbot would!*');
           commandIssued = true;
         }
       }
@@ -82,12 +83,36 @@ client.on('message', message => {
                 message.channel.send(message.mentions.users.first().avatarURL);
                 commandIssued = true;
               }
+            }else if (arg === 'ud' && args[index + 1]) {
+
+              var searchTerm = args.slice(1).join('+');
+
+              var udUrl = 'http://api.urbandictionary.com/v0/define?term=' + searchTerm;
+
+              commandIssued = true;
+              //console.log('commandIssued is now true');
+
+              http.get(udUrl, function(res){
+                var body = '';
+                res.on('data', function(chunk){
+                    body += chunk;
+                });
+                res.on('end', function(){
+                    var udResponse = JSON.parse(body);
+                    if (udResponse.result_type != 'no_results') {
+                      message.channel.send('__Urban Dictionary__\n**' + udResponse.list[0].word + ':**' + '```\n' + udResponse.list[0].definition + '\n```\n \n **example:**\n```' + udResponse.list[0].example + '\n```');
+                    }else {
+                      message.channel.send('No results for word or term.');
+                    }
+                });
+              }).on('error', function(e){console.log("Got an error: ", e);});
             }
           }
         });
       }
 
       if (commandIssued === false) {
+        //console.log('detecting commandIssued as false');
         askCleverBot(message.toString().slice(22), message);
       }
 
@@ -107,6 +132,8 @@ client.on('message', message => {
       message.reply('https://i.imgur.com/XkGB8qV.gif');
     }else if (msg.includes('green')) {
       message.reply('green is not a creative color');
+    }else if (msg.includes('ayy lmao')) {
+      message.reply('http://i.imgur.com/m7NaGVx.gif');
     }
 
   }
